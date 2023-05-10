149d0452aab2a38b4bbacd15ea2bbeeb32fa2c2f
Piastrelle <fabriziolisci34@gmail.com>
2023-05-10 20:04:56 +0200

    Ora funziona, worka su 1920 x 1080

--- AMQ_Destroyer/amq.py
+++ AMQ_Destroyer/amq.py
@@ -12,7 +12,7 @@ ANNULLA = PATH_AMQ_DESTROYER + "/images/annulla.png"
 BAND = PATH_AMQ_DESTROYER + "/images/band.png"
 SONG = PATH_AMQ_DESTROYER + "/images/song.png"
 
-TesseractPath = r'C:/Program Files/Tesseract-OCS/tesseract.exe'
+TesseractPath = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
 if not os.path.exists(TesseractPath):
     for root, dirs, files in os.walk(r'C:/'):
@@ -22,10 +22,8 @@ if not os.path.exists(TesseractPath):
         # comparing here like this
             if name == "tesseract.exe": 
                 print(os.path.abspath(os.path.join(root, name)))
-else:
-    print("esiste il file")
 
-loki.pytesseract.tesseract_cmd = "";
+loki.pytesseract.tesseract_cmd = TesseractPath
 
 with open(DATABASE, "r") as db:
     data = json.load(db)
@@ -35,33 +33,32 @@ anime = ""
 clicked = False
 
 while not keyboard.is_pressed("q"):
-    pag.screenshot(MARK, region=(690, 180, 60, 70))
+    pag.screenshot(MARK, region=(580, 110, 330, 60))
 
     while searching:
 
         if (loki.image_to_string(MARK) == ""):
             if not clicked:
-                pag.click(1690, 65)
+                pag.click(1705, 50)
                 clicked = True
                 time.sleep(3)
-                pag.screenshot(ANNULLA, region=(1615, 93, 80, 40))
+                pag.screenshot(ANNULLA, region=(1680, 85, 85, 40))
 
             if (pag.locateCenterOnScreen(ANNULLA) == None):
-                if (pag.locateCenterOnScreen(PATH_AMQ_DESTROYER + 'images/error.png') == None):
-                    pag.screenshot(BAND, region=(1300, 210, 400, 50))
-                    pag.screenshot(SONG, region=(1300, 160, 400, 50))
+                if (pag.locateCenterOnScreen(PATH_AMQ_DESTROYER + '/images/error.png') == None):
+                    pag.screenshot(BAND, region=(1350, 180, 410, 50))
+                    pag.screenshot(SONG, region=(1350, 130, 410, 50))
                     band = (loki.image_to_string(BAND)).replace('\n', '')
                     song = (loki.image_to_string(SONG)).replace('\n', '')
 
-                    pag.click(640, 695)
+                    pag.click(750, 600)
+
                     try:
-                        anime = (data['latin'][band.lower()][song.lower()])
+                        anime = (data[band.lower()][song.lower()])
                         print("trovato\n\t"+band+"\t"+song+"\t"+anime)
                     except:
                         print("non presente nel database:\n\t- "+band+"\t"+song)
-                        database = open(DATABASE, "a")
-                        database.write("\t\t\t\""+band+"\": {\n\t\t\t\t\""+song+"\": \""+anime+"\"")
-                        database.close()
+                        
                     pag.write(anime)
                     pag.press('enter')
                 
@@ -71,7 +68,7 @@ while not keyboard.is_pressed("q"):
 
                 searching = False
         else:
-            pag.screenshot(MARK, region=(690, 180, 60, 70))
+            pag.screenshot(MARK, region=(580, 110, 330, 60))
 
     while not searching:
         pag.screenshot(MARK, region=(690, 180, 60, 70))
@@ -80,7 +77,7 @@ while not keyboard.is_pressed("q"):
             clicked = False
         time.sleep(1)
 
-
+'''
 def appendToJson(keyword, keyword2, value, jsonFile):
     with open(PATH_AMQ_DESTROYER + jsonFile, "r") as file:
         data = json.loads(file.read())
@@ -94,3 +91,4 @@ def appendToJson(keyword, keyword2, value, jsonFile):
         file.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
 
 #appendToJson("lisa", "adamas", "Sword Art Online: Alicization", "/db.json")
+'''
