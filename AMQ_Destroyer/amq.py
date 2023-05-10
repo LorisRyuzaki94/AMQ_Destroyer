6159033b0d7c5f0f13a02115f20c1032bdbc8bd3
LorisRyuzaki94 <133097543+LorisRyuzaki94@users.noreply.github.com>
2023-05-10 10:27:30 +0200

    Add files via upload

---
+++ AMQ_Destroyer/amq.py
@@ -0,0 +1,95 @@
+import pyautogui as pag
+import time
+import pytesseract as loki
+import json
+import keyboard
+
+'''
+istruzioni
+
+installare pyautogui scrivendo nel cmd il comando
+    - py -m pip install pyautogui
+
+installare pytesseract scrivendo nel cmd il comando
+    - py -m pip install pytesseract
+
+installare keyboard scrivendo nel cmd il comando
+    - py -m pip install keyboard
+
+installare l'eseguibile di tesseract
+    https://github.com/UB-Mannheim/tesseract/wiki
+
+cambiare i path di AMQ_Destroyer
+
+modificare coordinate di
+    pag.screenshot
+    pag.click
+'''
+
+loki.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
+with open("C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/db.json", "r") as db:
+    data = json.load(db)
+
+searching = True
+anime = ""
+clicked = False
+
+while not keyboard.is_pressed("q"):
+    pag.screenshot('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/mark.png', region=(690, 180, 60, 70))
+
+    while searching:
+
+        if (loki.image_to_string('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/mark.png') == ""):
+            if not clicked:
+                pag.click(1690, 65)
+                clicked = True
+                time.sleep(3)
+                pag.screenshot('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/annulla.png', region=(1612, 93, 80, 40))
+
+            if (pag.locateCenterOnScreen('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/annulla.png') == None):
+                if (pag.locateCenterOnScreen('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/error.png') == None):
+                    pag.screenshot('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/band.png', region=(1300, 210, 400, 50))
+                    pag.screenshot('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/song.png', region=(1300, 160, 400, 50))
+                    band = (loki.image_to_string('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/band.png')).replace('\n', '')
+                    song = (loki.image_to_string('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/song.png')).replace('\n', '')
+
+                    pag.click(640, 695)
+                    try:
+                        anime = (data['latin'][band.lower()][song.lower()])
+                        print("trovato\n\t"+band+"\t"+song+"\t"+anime)
+                    except:
+                        print("non presente nel database:\n\t- "+band+"\t"+song)
+                    pag.write(anime)
+                    pag.press('enter')
+                
+                else:
+                    print("error\n\tno audio")
+                    pag.click(640, 695)
+
+                searching = False
+        else:
+            pag.screenshot('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/mark.png', region=(690, 180, 60, 70))
+
+    while not searching:
+        pag.screenshot('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/mark.png', region=(690, 180, 60, 70))
+        if (loki.image_to_string('C:/Users/user6_12_1/Desktop/AMQ_Bot/AMQ_Destroyer/images/mark.png') != ""):
+            searching = True
+            clicked = False
+        time.sleep(1)
+
+'''
+# Stampa le coordinate del cursore quando premi Q
+while True:
+    if keyboard.is_pressed('q'):
+        x, y = pag.position()
+        print(str(x)+", "+str(y))
+        time.sleep(1)
+'''
+'''
+da fare:
+    selezione del primo guess invece dell'invio per confermare
+    aggiunta di canzoni non conosciute quando "non presente"
+    traduzione dal giapponese del testo riconosciuto
+    errore nel riconoscere parole corte? (Redo, Zero!!)
+'''
