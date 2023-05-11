16db3a180306059323187784ccc8b4c39373942e
Piastrelle <fabriziolisci34@gmail.com>
2023-05-11 16:28:59 +0200

    Migliorie al sistema di coordinate e primi utlizzi del file settings.json

--- AMQ_Destroyer/amq.py
+++ AMQ_Destroyer/amq.py
@@ -16,10 +16,10 @@ TesseractPath = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 if not os.path.exists(TesseractPath):
     for root, dirs, files in os.walk(r'C:/'):
         for name in files:
-       
-          # As we need to get the provided python file,
-        # comparing here like this
-            if name == "tesseract.exe": 
+
+            # As we need to get the provided python file,
+            # comparing here like this
+            if name == "tesseract.exe":
                 print(os.path.abspath(os.path.join(root, name)))
 
 loki.pytesseract.tesseract_cmd = TesseractPath
@@ -33,10 +33,12 @@ with open(DATABASE, "r") as db:
 searching = True
 anime = ""
 clicked = False
-annullaRegion = [settings['shazam-position'][0] - 90, settings['shazam-position'][1] + 25, 85, 40]
+annullaRegion = [settings['shazam-position'][0] - 75, settings['shazam-position'][1] + 30, 85, 20]
+markRegion = [settings['mark-position'][0] - 165, settings['mark-position'][1] - 30, 330, 60]
+screenshotRegion = [settings['shazam-position'][0] - 410, settings['shazam-position'][1] + 30, 420, 180]
 
 while not keyboard.is_pressed("q"):
-    pag.screenshot(MARK, region=(580, 110, 330, 60))
+    pag.screenshot(MARK, region=markRegion)
 
     while searching:
 
@@ -45,12 +47,12 @@ while not keyboard.is_pressed("q"):
                 pag.click(settings['shazam-position'])
                 clicked = True
                 time.sleep(4)
-                pag.screenshot(ANNULLA, region=(annullaRegion))
+                pag.screenshot(ANNULLA, region=annullaRegion)
 
-            if pag.locateCenterOnScreen(ANNULLA) == None:
-                if pag.locateCenterOnScreen(PATH_AMQ_DESTROYER + '/images/error.png') == None:
-                    
-                    pag.screenshot(INFO, region=(1345, 100, 420, 150))
+            if not pag.locateCenterOnScreen(ANNULLA):
+                if not pag.locateCenterOnScreen(PATH_AMQ_DESTROYER + '/images/error.png'):
+
+                    pag.screenshot(INFO, region=screenshotRegion)
                     info = loki.image_to_string(INFO).split('\n')
                     while '' in info:
                         info.remove('')
@@ -61,50 +63,44 @@ while not keyboard.is_pressed("q"):
                     except:
                         print("errore\n\tproblema nel riconoscimento del testo")
 
-                    pag.click(750, 600)
+                    pag.click(settings['input-position'])
 
                     try:
                         anime = (data[band.lower()][song.lower()])
-                        print("trovato\n\t"+band+"\t"+song+"\t"+anime)
+                        print("trovato\n\t" + band + "\t" + song + "\t" + anime)
                     except:
-                        print("non presente nel database:\n\t- "+band+"\t"+song)
-                        
+                        print("non presente nel database:\n\t- " + band + "\t" + song)
+
                     pag.write(anime)
                     pag.press('enter')
-                
+
                 else:
                     print("error\n\tno audio")
-                    pag.click(640, 695)
+                    pag.click(settings['input-position'])
 
                 searching = False
         else:
-            pag.screenshot(MARK, region=(580, 110, 330, 60))
+            pag.screenshot(MARK, region=markRegion)
 
     while not searching:
-        pag.screenshot(MARK, region=(580, 110, 330, 60))
+        pag.screenshot(MARK, region=markRegion)
         if loki.image_to_string(MARK) != "":
             searching = True
             clicked = False
         else:
             time.sleep(1)
 
+
 def appendToJson(keyword, keyword2, value, jsonFile):
     with open(PATH_AMQ_DESTROYER + jsonFile, "r") as file:
         data = json.loads(file.read())
 
     if keyword not in data:
-        data[keyword] = {keyword2 : value}
-    else: 
-        data[keyword].update({keyword2 : value})
+        data[keyword] = {keyword2: value}
+    else:
+        data[keyword].update({keyword2: value})
 
     with open(PATH_AMQ_DESTROYER + jsonFile, 'w') as file:
         file.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
 
-#appendToJson("lisa", "adamas", "Sword Art Online: Alicization", "/db.json")
-
-
-
-
-print(settings['shazam-position'])
-
-
+# appendToJson("lisa", "adamas", "Sword Art Online: Alicization", "/db.json")
