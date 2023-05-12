edb98a566beb4264722aac3c25b78b2abd50efdc
Piastrelle <fabriziolisci34@gmail.com>
2023-05-12 10:33:46 +0200

    Implementata la funzione di scrittura sul database delle entry non riconosciute

--- AMQ_Destroyer/amq.py
+++ AMQ_Destroyer/amq.py
@@ -37,6 +37,7 @@ clicked = False
 annullaRegion = [settings['shazam-position'][0] - 75, settings['shazam-position'][1] + 30, 85, 20]
 markRegion = [settings['mark-position'][0] - 165, settings['mark-position'][1] - 30, 330, 60]
 screenshotRegion = [settings['shazam-position'][0] - 410, settings['shazam-position'][1] + 30, 420, 180]
+added = False
 
 
 def normalize(reading):
@@ -46,6 +47,21 @@ def normalize(reading):
             reading = reading.replace(char, replacement)
 
     return reading
+
+
+def appendToJson(keyword, keyword2, value, jsonFile):
+    with open(jsonFile, "r") as file:
+        data = json.loads(file.read())
+
+    if keyword not in data:
+        data[keyword] = {keyword2: value}
+    else:
+        data[keyword].update({keyword2: value})
+
+    with open(jsonFile, 'w') as file:
+        file.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+
+# appendToJson("lisa", "adamas", "Sword Art Online: Alicization", "/db.json")
 
 
 while not keyboard.is_pressed("q"):
@@ -61,33 +77,40 @@ while not keyboard.is_pressed("q"):
                 pag.screenshot(ANNULLA, region=annullaRegion)
 
             if not pag.locateCenterOnScreen(ANNULLA):
-                if not pag.locateCenterOnScreen(PATH_AMQ_DESTROYER + '/images/error.png'):
 
-                    pag.screenshot(INFO, region=screenshotRegion)
-                    info = loki.image_to_string(INFO).split('\n')
-                    while '' in info:
-                        info.remove('')
+                pag.screenshot(INFO, region=screenshotRegion)
+                info = loki.image_to_string(INFO).split('\n')
+                while '' in info:
+                    info.remove('')
+                try:
+                    band = info[len(info) - 1]
+                    song = info[0]
+                except:
+                    print("errore\n\tproblema nel riconoscimento del testo")
+                pag.click(settings['input-position'])
 
-                    try:
-                        band = info[len(info) - 1]
-                        song = info[0]
-                    except:
-                        print("errore\n\tproblema nel riconoscimento del testo")
+                try:
+                    anime = (data[normalize(band.lower())][normalize(song.lower())])
+                    print("trovato\n\t" + band + "\t" + song + "\t" + anime)
+                except:
+                    if not pag.locateCenterOnScreen(PATH_AMQ_DESTROYER + '/images/error.png'):
+                        while not added:
+                            time.sleep(1)
+                            pag.screenshot(MARK, region=markRegion)
+                            if not loki.image_to_string(MARK) == "":
+                                appendToJson(normalize(band).lower(), normalize(song).lower(), loki.image_to_string(MARK).replace('\n', " "), DATABASE)
+                                print("aggiunto al database:\n\t- " + band + "\t" + song + "\t" + loki.image_to_string(MARK))
+                                added = True
+                            else:
+                                print("error\n\tno audio")
+                                pag.click(settings['input-position'])
+                    else:
+                        print("error\n\tno audio")
+                        pag.click(settings['input-position'])
 
-                    pag.click(settings['input-position'])
 
-                    try:
-                        anime = (data[normalize(band.lower())][normalize(song.lower())])
-                        print("trovato\n\t" + band + "\t" + song + "\t" + anime)
-                    except:
-                        print("non presente nel database:\n\t- " + band + "\t" + song)
-
-                    pag.write(anime)
-                    pag.press('enter')
-
-                else:
-                    print("error\n\tno audio")
-                    pag.click(settings['input-position'])
+                pag.write(anime)
+                pag.press('enter')
 
                 searching = False
         else:
@@ -98,20 +121,8 @@ while not keyboard.is_pressed("q"):
         if loki.image_to_string(MARK) != "":
             searching = True
             clicked = False
+            added = False
+
         else:
             time.sleep(0.1)
 
-
-def appendToJson(keyword, keyword2, value, jsonFile):
-    with open(PATH_AMQ_DESTROYER + jsonFile, "r") as file:
-        data = json.loads(file.read())
-
-    if keyword not in data:
-        data[keyword] = {keyword2: value}
-    else:
-        data[keyword].update({keyword2: value})
-
-    with open(PATH_AMQ_DESTROYER + jsonFile, 'w') as file:
-        file.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
-
-# appendToJson("lisa", "adamas", "Sword Art Online: Alicization", "/db.json")
