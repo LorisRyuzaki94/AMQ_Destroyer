7e8bcd286ba0ff4c89020eac043b58ebfdd05d7f
Piastrelle <fabriziolisci34@gmail.com>
2023-06-02 03:33:27 +0200

    Aggiunti commenti esplicativi di ciò che fa il codice. Eliminato un file vuoto

--- AMQ_DestroyerJavascript/background.js
+++ AMQ_DestroyerJavascript/background.js
@@ -1,17 +1,17 @@
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) { // Listener che ascolta il messaggio inviato da content.js
 
-    if (message.action === "check-code") {
-        fetch(new Request("database.json"))
+    if (message.action === "check-code") { // Se il messaggio è di tipo "check-code"
+        fetch(new Request("database.json")) // Legge il file database.json
             .then((response) => response.json())
             .then((database) => {
-                anime = database[message.code];
-                if (anime === undefined) {
+                anime = database[message.code]; // Cerca nel database una corrispondenza per il codice ricevuto
+                if (anime === undefined) { // Se non lo trova restituisce il codice
                     anime = message.code;
                 }
-                sendResponse({name: anime});
+                sendResponse({name: anime}); // Invia la risposta al file content.js
             }
         );
     }
-    return true;
+    return true; // Questo serve altrimenti non funziona bene
 });
--- AMQ_DestroyerJavascript/content.js
+++ AMQ_DestroyerJavascript/content.js
@@ -8,16 +8,16 @@ chrome.tabs.query({ active: true }, function (tabs) {
             },
 
             func: () => {
-                if (document.getElementById("qpMoePlayer-0").classList.contains("vjs-paused")) {
+                if (document.getElementById("qpMoePlayer-0").classList.contains("vjs-paused")) { // Cerca e seleziona quale dei due video player sia attivo
                     id = "qpMoePlayer-1_html5_api";
                 } else {
                     id = "qpMoePlayer-0_html5_api";
                 }
-                var link = document.getElementById(id).src;
-                code = link.replace("https://amq.catbox.video/", "").replace(".mp3", "");
+                var link = document.getElementById(id).src; // Cerca l'elemento contrassegnato dall'id univoco e ottiene il campo src (link del video)
+                code = link.replace("https://amq.catbox.video/", "").replace(".mp3", ""); // estrae il codice dal link
 
-                chrome.runtime.sendMessage({action: "check-code", code: code}, function(response) {
-                    document.getElementById("qpAnswerInput").value = response.name;
+                chrome.runtime.sendMessage({action: "check-code", code: code}, function(response) { // Invia un messaggio di tipo "check-code" con il codice. Il listener è in background.js
+                    document.getElementById("qpAnswerInput").value = response.name; // Ottiene la risposta da background.js e la imposta come valore del campo input
                 });
             }
         }
