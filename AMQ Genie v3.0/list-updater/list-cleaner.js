6b5aca50a0caa26e6c67d0dcecd6b9ed71cb64e9
Piastrelle <fabriziolisci34@gmail.com>
2024-06-15 05:10:47 +0200

    added list-cleaner tool

---
+++ AMQ Genie v3.0/list-updater/list-cleaner.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+// Leggi il file JSON
+fs.readFile('list-updater/list.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Errore nella lettura del file:', err);
+        return;
+    }
+
+    // Parse il contenuto del file JSON
+    let jsonData = JSON.parse(data);
+
+    // Rimuovi le proprietà specificate
+    jsonData = jsonData.map(item => {
+        const {
+            annId, annSongId, animeJPName, animeAltName, animeVintage, animeType, 
+            songType, songDifficulty, songCategory, HQ, MQ, artists, composers, arrangers,
+            ...rest
+        } = item;
+        return rest;
+    });
+
+    // Scrivi il nuovo JSON in un file
+    fs.writeFile('list-updater/list.json', JSON.stringify(jsonData, null, 2), 'utf8', err => {
+        if (err) {
+            console.error('Errore nella scrittura del file:', err);
+            return;
+        }
+        console.log('File JSON processato e salvato con successo come output.json');
+    });
+});
